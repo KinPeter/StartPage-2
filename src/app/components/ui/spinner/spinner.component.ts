10276ee7f2bc2bbcab89b11bd33feb6e1d11c45f
Peter Kin <kinpeter85@gmail.com>
2019-08-14 22:01:59 +0200

    Change spinner template

--- src/app/components/ui/spinner/spinner.component.ts
+++ src/app/components/ui/spinner/spinner.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-spinner',
-  templateUrl: './spinner.component.html',
-  styleUrls: ['./spinner.component.scss']
+    selector: 'app-spinner',
+    template: `
+        <div class="lds-ripple">
+            <div></div>
+            <div></div>
+        </div>
+    `,
+    styleUrls: ['./spinner.component.scss']
 })
 export class SpinnerComponent implements OnInit {
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
 }
