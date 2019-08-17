47a85a33d496eacc6096e11bf98e9a897d1ff2e7
Peter Kin <kinpeter85@gmail.com>
2019-08-17 16:51:38 +0200

    Add firebase init

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
+import { firebaseApiKey } from 'keys';
+
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: firebaseApiKey,
+    authDomain: 'p-kin-startpage.firebaseapp.com',
+    databaseURL: 'https://p-kin-startpage.firebaseio.com',
+    projectId: 'p-kin-startpage',
+    storageBucket: '',
+    messagingSenderId: '527940148035',
+    appId: '1:527940148035:web:4e28d49996dad044'
+  }
 };
