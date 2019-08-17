23cf2404e1a1a3b041651331e6646b2cdc0746ef
Peter Kin <kinpeter85@gmail.com>
2019-08-17 08:49:22 +0200

    Add new notes functionalities

--- src/app/components/notes/note/note.component.ts
+++ src/app/components/notes/note/note.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Note } from 'src/app/interfaces/note';
 import { NotesService } from 'src/app/services/notes.service';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
     selector: 'app-note',
@@ -10,14 +11,19 @@ import { NotesService } from 'src/app/services/notes.service';
 export class NoteComponent implements OnInit {
 
     @Input() note: Note;
+    isLoggedIn: boolean;
 
     constructor(
-        public noteService: NotesService
+        public noteService: NotesService,
+        public auth: AuthService
     ) {
 
     }
 
     ngOnInit() {
+        this.auth.loggedIn.subscribe((value) => {
+            this.isLoggedIn = value;
+        });
     }
 
     editNote(): void {
@@ -25,11 +31,11 @@ export class NoteComponent implements OnInit {
     }
 
     archiveNote(): void {
-
+        this.noteService.archiveNote(this.note.id, this.note.archived);
     }
 
     deleteNote(): void {
-
+        this.noteService.deleteNote(this.note.id);
     }
 
 }
--- src/app/services/notes.service.ts
+++ src/app/services/notes.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
-import { AngularFirestore, QuerySnapshot, QueryDocumentSnapshot } from '@angular/fire/firestore';
-import { Observable } from 'rxjs';
+import { AngularFirestore, QuerySnapshot, QueryDocumentSnapshot, DocumentReference } from '@angular/fire/firestore';
+import { Observable, Subject } from 'rxjs';
 import { Note } from '../interfaces/note';
+import { AlertService } from './alert.service';
+import { SpinnerService } from './spinner.service';
 
 @Injectable({
     providedIn: 'root'
@@ -9,16 +11,30 @@ import { Note } from '../interfaces/note';
 export class NotesService {
 
     public notesCollection = this.db.collection<Note>('notes');
-    public notes: Observable<QuerySnapshot<any>>;
+    public notes: Subject<QuerySnapshot<any>>;
 
     constructor(
-            public db: AngularFirestore
+            public db: AngularFirestore,
+            public alert: AlertService,
+            public spinner: SpinnerService
         ) {
+        this.notes = new Subject();
         this.fetchNotes();
     }
 
     fetchNotes(): void {
-        this.notes = this.db.collection<Note>('notes', (ref) => ref.orderBy('added', 'desc')).get();
+        this.db.collection<Note>('notes', (ref) => ref.orderBy('added', 'desc')).get()
+        .toPromise()
+        .then((data: QuerySnapshot<any>) => {
+            if (data.empty) {
+                this.notes.error(new Error('No notes found.'));
+            } else {
+                this.notes.next(data);
+            }
+        })
+        .catch((error) => {
+            this.notes.error(error);
+        });
     }
 
     distributeAndSortNotes(data: QuerySnapshot<any>): Note[] {
@@ -38,5 +54,55 @@ export class NotesService {
         return active.concat(archived);
     }
 
+    addNewNote(note: Note): void {
+        this.spinner.show();
+        this.notesCollection.add(note)
+        .then((response: DocumentReference) => {
+            this.alert.show('Note added successfully.', 'success');
+            this.fetchNotes();
+            console.log(response);
+        })
+        .catch((error) => {
+            console.log(error);
+            this.alert.show('Error adding note. ' + error.message, 'danger');
+        })
+        .finally(() => {
+            this.spinner.hide();
+        });
+    }
+
+    deleteNote(id: string): void {
+        this.spinner.show();
+        this.notesCollection.doc(id).delete()
+        .then(() => {
+            this.alert.show('Note deleted successfully.', 'success');
+            this.fetchNotes();
+        })
+        .catch((error) => {
+            console.log(error);
+            this.alert.show('Error deleting note. ' + error.message, 'danger');
+        })
+        .finally(() => {
+            this.spinner.hide();
+        });
+    }
+
+    archiveNote(id: string, archived: boolean): void {
+        this.spinner.show();
+        this.notesCollection.doc(id).update({
+            archived: !archived
+        })
+        .then(() => {
+            this.alert.show('Note updated successfully.', 'success');
+            this.fetchNotes();
+        })
+        .catch((error) => {
+            console.log(error);
+            this.alert.show('Error updating note. ' + error.message, 'danger');
+        })
+        .finally(() => {
+            this.spinner.hide();
+        });
+    }
 
 }
