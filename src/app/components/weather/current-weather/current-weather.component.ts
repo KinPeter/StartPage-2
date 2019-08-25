b7fa89bbcbf7408328ac35ef357400620522d069
Peter Kin <kinpeter85@gmail.com>
2019-08-25 14:37:46 +0200

    Outsource to currentWeather component

--- src/app/components/weather/current-weather/current-weather.component.ts
+++ src/app/components/weather/current-weather/current-weather.component.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { CurrentWeather } from 'src/app/interfaces/weather';
 
 @Component({
-  selector: 'app-current-weather',
-  templateUrl: './current-weather.component.html',
-  styleUrls: ['./current-weather.component.scss']
+    selector: 'app-current-weather',
+    templateUrl: './current-weather.component.html',
+    styleUrls: ['./current-weather.component.scss']
 })
 export class CurrentWeatherComponent implements OnInit {
 
-  constructor() { }
+    @Input() weather: CurrentWeather;
+    public isHighTemp = false;
+    public isLowTemp = false;
 
-  ngOnInit() {
-  }
+    constructor() { }
+
+    ngOnInit() {
+        if (this.weather) {
+            this.isHighTemp = this.weather.temperature > 28;
+            this.isLowTemp = this.weather.temperature < 8;
+        }
+    }
+
+    getWindScale(wSpeed: number): string {
+        let wScale = '';
+        if (wSpeed > 1 && wSpeed <= 4) {
+            wScale = 'Light breeze';
+        } else if (wSpeed > 4 && wSpeed <= 9) {
+            wScale = 'Light wind';
+        } else if (wSpeed > 9 && wSpeed <= 13) {
+            wScale = 'Moderate wind';
+        } else if (wSpeed > 13 && wSpeed <= 19) {
+            wScale = 'Strong wind';
+        } else if (wSpeed > 19 && wSpeed <= 24) {
+            wScale = 'Stormy wind';
+        } else if (wSpeed > 24) {
+            wScale = 'Crazy wind';
+        }
+        return wScale;
+    }
 
 }
--- src/app/components/weather/weather.component.ts
+++ src/app/components/weather/weather.component.ts
@@ -12,8 +12,6 @@ export class WeatherComponent implements OnInit {
     public weather: Weather;
     public city: string;
     public today: Date;
-    public isHighTemp = false;
-    public isLowTemp = false;
 
     constructor(
         public weatherService: WeatherService
@@ -22,33 +20,11 @@ export class WeatherComponent implements OnInit {
     ngOnInit() {
         this.weatherService.weather.subscribe((value: Weather) => {
             this.weather = value;
-            if (value) {
-                this.isHighTemp = value.currentWeather.temperature > 28;
-                this.isLowTemp = value.currentWeather.temperature < 8;
-            }
         });
         this.weatherService.city.subscribe((value: string) => {
             this.city = value;
         });
         this.today = new Date();
-    }
-
-    getWindScale(wSpeed: number): string {
-        let wScale = '';
-        if (wSpeed > 1 && wSpeed <= 4) {
-            wScale = 'Light breeze';
-        } else if (wSpeed > 4 && wSpeed <= 9) {
-            wScale = 'Light wind';
-        } else if (wSpeed > 9 && wSpeed <= 13) {
-            wScale = 'Moderate wind';
-        } else if (wSpeed > 13 && wSpeed <= 19) {
-            wScale = 'Strong wind';
-        } else if (wSpeed > 19 && wSpeed <= 24) {
-            wScale = 'Stormy wind';
-        } else if (wSpeed > 24) {
-            wScale = 'Crazy wind';
-        }
-        return wScale;
     }
 
 }
