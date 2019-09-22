b0c73a41b8b6b1fb992b23ce03d1d3cdea7aa4ed
Peter Kin <kinpeter85@gmail.com>
2019-09-22 08:04:22 +0200

    Add new tag to dev.to

--- src/app/components/dev-to/dev-to.component.ts
+++ src/app/components/dev-to/dev-to.component.ts
@@ -13,6 +13,7 @@ export class DevToComponent implements OnInit {
     public tags = [
         'javascript',
         'angular',
+        'vue',
         'react',
         'typescript',
         'css',
